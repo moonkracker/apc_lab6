071b1d416510b2bc0a7227210832f78a7a55705e
Maxim Kononovich <36278912+moonkracker@users.noreply.github.com>
2020-05-07 19:57:50 +0300

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -9,6 +9,34 @@ void SetMask(unsigned char mask);
 int commandIsExecuted;
 int quitFlag;
 int needHighlight;
+typedef unsigned char byte;
+
+struct VIDEO
+{
+	byte symb;
+	byte attr;
+};
+
+const int defaultAttr = 0x20;							//зеленый цвет
+
+void printRegister(int value)
+{
+	VIDEO far* screen = (VIDEO far*)MK_FP(0xB800, 0);
+	for (int i = 1; i >= 0; i--)
+	{
+		int currBits = ((value >> (i * 4)) & 0x0F);
+		if (currBits < 10)
+		{
+			screen->symb = currBits + '0';
+		}
+		else
+		{
+			screen->symb = currBits - 10 + 'A';
+		}
+		screen->attr = defaultAttr;
+		screen++;
+	}
+}
 
 void main()
 {
@@ -20,54 +48,54 @@ void main()
 	setvect(9, NewInterrupt);
 	while (!quitFlag)
 	{
-			if (needHighlight)
-			{
-				SetMask(0x04);                                      //Включение светодиода capslock
-				delay(100);
-     			SetMask(0x00);	
-				delay(100);
-     			SetMask(0x04);	
-				delay(1000);
-     			SetMask(0x00);		
-				delay(5000);
-				SetMask(0x04);
-				delay(10);
-				SetMask(0x00);	
-				needHighlight = 0;
-			}
+		if (needHighlight)
+		{
+			SetMask(0x04);                                      //Включение светодиода capslock
+			delay(100);
+			SetMask(0x00);
+			delay(100);
+			SetMask(0x04);
+			delay(1000);
+			SetMask(0x00);
+			delay(2000);
+			SetMask(0x04);
+			delay(5000);
+			SetMask(0x00);
+			needHighlight = 0;
+		}
 	}
 	setvect(9, oldInterrupt);
 	return;
 }
+
 void interrupt NewInterrupt(...)
 {
-		unsigned char value = 0;
-		oldInterrupt();
-		value = inp(0x60);
-		if (value == 0x01)                                    // Выход из программы по escape
-			quitFlag = 1; 
-		UpdateHighlightFlag(value);
-		commandIsExecuted = commandIsExecuted || (needHighlight == 0) || (value == 0xFA);
-		printf("%x\t", value);
-		outp(0x20, 0x20);
+	unsigned char value = 0;
+	value = inp(0x60);
+	if (value == 0x01)                                    // Выход из программы по escape
+		quitFlag = 1;
+	UpdateHighlightFlag(value);
+	commandIsExecuted = commandIsExecuted || (needHighlight == 0) || (value == 0xFA);
+	printRegister(inp(0x60)); 
+	oldInterrupt();
 }
 void UpdateHighlightFlag(unsigned char value)
 {
-		if (value != 0x1c)                                   // Ожидание клавиши enter 
-			return; 
-		if (needHighlight)                        
-				needHighlight = 0;
-		else
-				needHighlight = 1;                                // Разрешаем blinking
+	if (value != 0x1c)                                   // Ожидание клавиши enter 
+		return;
+	if (needHighlight)
+		needHighlight = 0;
+	else
+		needHighlight = 1;                                // Разрешаем blinking
 }
 void SetMask(unsigned char mask)
 {
 	commandIsExecuted = 0;
-		while (!commandIsExecuted)                            // Пока нет подтверждения успешного выполнения команды
-		{
-				while ((inp(0x64) & 0x02) != 0x00);               // Ожидаем освобождения входного буфера клавиатуры
-				outp(0x60, 0xED);                                 // Записываем в порт команду управления индикаторами
-				outp(0x60, mask);                                 // Записываем в порт битовую маску для настройки индикаторов
-				delay(50);
-		}
+	while (!commandIsExecuted)                            // Пока нет подтверждения успешного выполнения команды
+	{
+		while ((inp(0x64) & 0x02) != 0x00);               // Ожидаем освобождения входного буфера клавиатуры
+		outp(0x60, 0xED);                                 // Записываем в порт команду управления индикаторами
+		outp(0x60, mask);                                 // Записываем в порт битовую маску для настройки индикаторов
+		delay(50);
+	}
 }
